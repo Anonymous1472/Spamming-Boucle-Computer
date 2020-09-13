25ea9c999b6a159356f609d659b501af746ffcf3
Mr_Size <58605590+Anonymous1472@users.noreply.github.com>
2020-09-13 21:55:00 +0200

    Add files via upload

---
+++ boucles.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+
+compteur = 0 #Variable
+
+while compteur < 5: #Utilisation de While (tant) "Tant que 'Compteur' est inférieur à 5, ne pas arreter"
+	print("Spam By Mr_Size !") #Affichage
+	compteur += 1 #Ajout au compteur
+
+print("\n") #Affichage
+print("Déroule le code dans ton éditeur de texte afin de voir les commentaires !") #Affichage
+
+
+compteur1 = 0
+
+while compteur1 < 10:
+	print("\n")
+	compteur1 += 1
+print("By T-Force One Mr_Size")
+
+
+
+
+
+input()
